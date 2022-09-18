97b44dcd88e5e04dbc34c78796336fbdefe975a2
lucl13 <lucl13@163.com>
2022-09-18 18:16:54 -0400

    gmx auto run v2

---
+++ Gromacs/1_autorun_gmx/V2/02_protein_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3             
+
+import subprocess    
+
+names = ['apo_4XYK']         
+
+
+for name in names:         
+    for i in range(1,2):    
+        subprocess.run(f'cp topol.top {i}_{name}_topol.top', shell=True) 
+        subprocess.run(f'echo "protein" | gmx editconf -f {i}_{name}.pdb -o {i}_{name}_box.pdb -c -d 1.2 -princ', shell=True)                             ##. !!修改02 盒子大小
+        subprocess.run(f'gmx solvate -cp {i}_{name}_box.pdb -cs spc216.gro -p {i}_{name}_topol.top -o {i}_{name}_sol.pdb', shell=True)
+        subprocess.run(f'gmx grompp -f ions.mdp -c {i}_{name}_sol.pdb -p {i}_{name}_topol.top -o {i}_{name}_ions.tpr -maxwarn 1', shell=True) 
+        subprocess.run(f'echo "sol\n" | gmx genion -s {i}_{name}_ions.tpr -o {i}_{name}_ions.pdb -neutral -p {i}_{name}_topol.top', shell=True) 
+        subprocess.run(f"sed -ie 's/CL/CLA/g' {i}_{name}_topol.top".format(i), shell=True)
+        subprocess.run(f'gmx grompp -f mini.mdp -c {i}_{name}_ions.pdb  -p {i}_{name}_topol.top -o {i}_{name}_em.tpr -maxwarn 1', shell=True) 
+        subprocess.run(f'gmx mdrun -v -deffnm {i}_{name}_em'.format(i), shell=True) 
+
+exit()
+
+
---
+++ Gromacs/1_autorun_gmx/V2/03_auto_run-NVT_NPT_MD.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3   
+
+import subprocess 
+
+md_time = 50 # ns
+md_steps = int(md_time*1e6/2)  
+   
+names = ['apo_4XYK']   
+
+def if_job_finished(job):
+    filename = job + '.log'
+    try:
+        tail_output = subprocess.check_output(['tail', filename])
+        last_line = tail_output.strip().decode("utf-8").split('\n')[-1]
+        return last_line.startswith('Finished')
+    except:
+        return False
+
+def run_NVT(i, name):
+    subprocess.run(f'gmx grompp -f nvt.mdp -c {i}_{name}_em.gro -r {i}_{name}_em.gro -p {i}_{name}_topol.top -o {i}_{name}_nvt.tpr', shell=True) 
+    p = subprocess.run(f'gmx mdrun -v -deffnm {i}_{name}_nvt', shell=True) 
+    if p.returncode == 0:
+        print(f'{i}_{name} NVT job Done!')
+    else: 
+        raise Exception(f'{i}_{name} NVT job Exception !!!') 
+
+def run_NPT(i, name):
+    subprocess.run(f'gmx grompp -f npt.mdp -c {i}_{name}_nvt.gro -r {i}_{name}_nvt.gro -t {i}_{name}_nvt.cpt -p {i}_{name}_topol.top -o {i}_{name}_npt.tpr -maxwarn 1', shell=True) 
+    p = subprocess.run(f'gmx mdrun -v -deffnm {i}_{name}_npt', shell=True) 
+    if p.returncode == 0:
+        print(f'{i}_{name} NPT job Done!')
+    else: 
+        raise Exception(f'{i}_{name} NPT job Exception !!!') 
+
+def run_MD(i, name, md_steps):
+    subprocess.run(f"sed -ie 's/nsteps     = .*/nsteps     = {md_steps}/g' md.mdp", shell=True)
+    subprocess.run(f'gmx grompp -f md.mdp -c {i}_{name}_npt.gro -r {i}_{name}_npt.gro -t {i}_{name}_npt.cpt -p {i}_{name}_topol.top -o {i}_{name}_md.tpr', shell=True) 
+    p = subprocess.run(f'gmx mdrun -v -deffnm {i}_{name}_md', shell=True) 
+    if p.returncode == 0:
+        print(f'{i}_{name} MD job Done!')
+    else: 
+        raise Exception(f'{i}_{name} MD job Exception !!!') 
+
+
+for name in names:         
+    for i in range(1,2):  
+
+        if if_job_finished(f'{i}_{name}_md'):
+            print('MD finished! Noting to do')
+
+        else:
+            if if_job_finished(f'{i}_{name}_npt'):
+
+                print('Running MD .....')
+                run_MD(i, name, md_steps)
+            else:
+
+                if if_job_finished(f'{i}_{name}_nvt'): 
+
+                    print('Running NPT .....')
+                    run_NPT(i, name)
+
+                    print('Running MD .....')
+                    run_MD(i, name, md_steps)
+
+                else:
+
+                    print('Running NVT .....')
+                    run_NVT(i, name)
+
+                    print('Running NPT .....')
+                    run_NPT(i, name)
+
+                    print('Running MD .....')
+                    run_MD(i, name, md_steps)
+
+exit()
+
+
+
+
