9d2b795df1c711e686ae77aae8dae8b0bf3f2fb8
lucl13 <lucl13@163.com>
2022-09-29 21:19:39 -0400

    add fix traj script

---
+++ Gromacs/1_autorun_gmx/V2/04_fix-trj.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import subprocess
+
+names = ['apo_4XYK']   
+
+for name in names:         
+    for i in range(1,2):  
+		subprocess.run(f'echo "protein system"| gmx trjconv -f {i}_{name}.xtc -s {i}_{name}.tpr -o cluster_{i}.xtc -pbc cluster', shell=True)
+		subprocess.run(f'echo "protein system" | gmx trjconv -f cluster_{i}.xtc -s {i}_{name}.tpr -o fit_{i}.xtc -fit rot+trans' , shell=True)
+		subprocess.run(f'echo "non-water"| gmx trjconv -f fit_{i}.xtc -s {i}_{name}.tpr   -o {i}_{name}_nowat.xtc -pbc mol', shell=True)
+		subprocess.run(f'echo "non-water" | gmx trjconv -f {i}_em.gro   -s {i}_{name}.tpr  -o {i}_{name}_nowat.gro', shell=True) 
+		
