9f504b7cf00c0563705e576af3ed1302b7d785db
lucl13 <lucl13@163.com>
2022-10-31 10:36:19 -0400

    fix 04_fix_trj

--- Gromacs/1_autorun_gmx/V2/04_fix-trj.py
+++ Gromacs/1_autorun_gmx/V2/04_fix-trj.py
@@ -2,12 +2,13 @@
 
 import subprocess
 
-names = ['apo_4XYK']   
+names = ['apo_2PFK']   
 
-for name in names:         
-    for i in range(1,2):  
-		subprocess.run(f'echo "protein system"| gmx trjconv -f {i}_{name}.xtc -s {i}_{name}.tpr -o cluster_{i}.xtc -pbc cluster', shell=True)
-		subprocess.run(f'echo "protein system" | gmx trjconv -f cluster_{i}.xtc -s {i}_{name}.tpr -o fit_{i}.xtc -fit rot+trans' , shell=True)
-		subprocess.run(f'echo "non-water"| gmx trjconv -f fit_{i}.xtc -s {i}_{name}.tpr   -o {i}_{name}_nowat.xtc -pbc mol', shell=True)
-		subprocess.run(f'echo "non-water" | gmx trjconv -f {i}_em.gro   -s {i}_{name}.tpr  -o {i}_{name}_nowat.gro', shell=True) 
+for name in names:
+	for i in range(1,2):
+		subprocess.run(f'echo "protein system"| gmx trjconv -f {i}_{name}_md.xtc -s {i}_{name}_md.tpr -o {i}_{name}_cluster.xtc -pbc cluster', shell=True)
+		subprocess.run(f'echo "protein system" | gmx trjconv -f {i}_{name}_cluster.xtc -s {i}_{name}_md.tpr -o {i}_{name}_fit.xtc -fit rot+trans' , shell=True)
+		subprocess.run(f'echo "non-water"| gmx trjconv -f {i}_{name}_fit.xtc -s {i}_{name}_md.tpr   -o {i}_{name}_nowat.xtc -pbc mol', shell=True)
+		subprocess.run(f'echo "non-water" | gmx trjconv -f {i}_{name}_em.gro   -s {i}_{name}_md.tpr  -o {i}_{name}_nowat.gro', shell=True) 
+		
 		
