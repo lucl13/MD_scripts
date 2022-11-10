c2ed615d7261c4a3942ea4a7a277063d21dd1f31
lucl13 <lucl13@163.com>
2022-11-10 15:03:11 -0500

    add logger to backrub

--- Rosetta/backrub/backrub_np.py
+++ Rosetta/backrub/backrub_np.py
@@ -8,9 +8,19 @@ import pandas as pd
 import pyrosetta
 import multiprocessing
 import time
+import sys
+import pyrosetta_help as ph
+import re
 
-def runBackrub(pose, job_id):
 
+
+def runBackrub(file, job_id):
+
+    job_name = f'{file[:-4]}_{int(job_id):05}'
+    logger = ph.configure_logger()
+    pyrosetta.init('-backrub:ntrials 100 -out:overwrite true -extra_res_fa RIP.params -ignore_zero_occupancy false')
+
+    pose = pyrosetta.pose_from_pdb(file)
     tf = pyrosetta.rosetta.core.pack.task.TaskFactory()
     tf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
     tf.push_back(pyrosetta.rosetta.core.pack.task.operation.IncludeCurrent())
@@ -22,8 +32,6 @@ def runBackrub(pose, job_id):
     mm.set_chi(True)
     mm.set_jump(True)
 
-    OUTPUT_DIR = './output_3/'
-
     backrub = pyrosetta.rosetta.protocols.backrub.BackrubProtocol()
     backrub.set_taskfactory(tf)
     backrub.set_movemap(mm)
@@ -31,24 +39,34 @@ def runBackrub(pose, job_id):
 
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
-    pose.dump_pdb(f'{OUTPUT_DIR}/1_apo_1urp_backrubed_{int(job_id):05}.pdb')
+    pose.dump_pdb(f'{OUTPUT_DIR}/{job_name}.pdb')
 
-    score_df = pd.DataFrame()
     score_dict = dict(pose.scores)
     score_dict['description'] = f'backrub_{int(job_id):05}'
-    score_df = score_df.append(score_dict, ignore_index=True)
+    score_df = score_df = pd.DataFrame(score_dict, index=[0])
     score_scv = f'{OUTPUT_DIR}/score_backub.csv'
     if os.path.isfile(score_scv):
         score_df.to_csv(score_scv, mode='a', header=False, index=False)
     else:
         score_df.to_csv(score_scv, index=False)
 
+    entries = ph.get_all_log_entries()
+    log_text = [str(e['text']) for e in entries]
+
+    with open(f'{OUTPUT_DIR}/{job_name}.log', 'w') as f:
+        for line in log_text:
+            ansi_escape = re.compile(r'\x1b(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+            line_cleaned = ansi_escape.sub('', line)
+            f.write(f"{line_cleaned}\n")
+
 
 def runBackrubs(file, job_ids):
-    pyrosetta.init('-backrub:ntrials 10000', '-out:overwrite')
-    pose = pyrosetta.pose_from_pdb(file)
     for job_id in job_ids:
-        runBackrub(pose, job_id)
+        #ph.get_all_log_entries()
+
+        runBackrub(file, job_id)
+
+
 
 
 def make_chunks(data: list, thread_count) -> dict:
@@ -75,8 +93,11 @@ def make_chunks(data: list, thread_count) -> dict:
 if __name__ == '__main__':
 
 
-    file ='1_apo_1urp_relaxed.pdb'
-    n_sctrut = 10000
+    file ='2dri-RIP_relaxed.pdb'
+    n_sctrut = 22
+    
+    global OUTPUT_DIR 
+    OUTPUT_DIR = './output_2/'
 
     job_ids = [i for i in range(1, n_sctrut+1)]
     chunks = make_chunks(job_ids, 22)
